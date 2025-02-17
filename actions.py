0d432d0ac502072129602892e09d87df33891ac5
Amir Khan <39916472+amirkh69@users.noreply.github.com>
2025-02-17 11:10:04 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -5,6 +5,6 @@ def get_seo_page_report(url :str):
     api_params = {
         'url': url,
     }
-    api_client = RapidAPIClient(api_key="28fa374bb3mshcee546e09fac28ep11043bjsn1d6e5b460873") 
+    api_client = RapidAPIClient(api_key="Rapid-API-Key") 
     response = api_client.call_api(api_url, method='GET', params=api_params)
     return response
